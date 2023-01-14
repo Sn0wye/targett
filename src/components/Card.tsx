27645318cc4c2825ca749410b1fe206a1688d563
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-01-14 18:14:36 -0300

    feat: add update and refine deletion

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -12,49 +12,65 @@ import { Goal, useGoals } from '../hooks/useGoals';
 import { Actions } from './Actions';
 import { ConfirmationModal } from './ConfirmationModal';
 import { GoalCount } from './GoalCount';
+import { Edit } from './Icons/Edit';
 import { Trash } from './Icons/Trash';
+import { UpdateGoalModal } from './UpdateGoalModal';
 type CardProps = {
   goal: Goal;
 };
 
 export const Card = ({ goal }: CardProps) => {
   const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
+  const [isUpdateModalOpen, setIsUpdateModalOpen] = useState(false);
   const { current, total, name, id } = goal;
 
   const { width } = Dimensions.get('window');
 
-  const deleteXThreshold = width * 0.2;
+  const gestureThreshold = width * 0.2;
+  const deleteXThreshold = -gestureThreshold; // Swipe left to delete
+  const updateXThreshold = gestureThreshold; // Swipe right to update
 
   const translateX = useSharedValue(0);
   const marginTop = useSharedValue(8);
   const itemHeight = useSharedValue(200);
   const iconOpacity = useSharedValue(1);
 
-  const { deleteGoal, updateGoal } = useGoals();
+  const { deleteGoal, updateCurrent } = useGoals();
 
   const gesture = useMemo(
     () =>
       Gesture.Pan()
         .onUpdate(e => {
-          translateX.value = e.translationX < 0 ? e.translationX : 0; // TODO: remove this to use left gesture
+          translateX.value = e.translationX;
         })
         .onEnd(e => {
-          const shouldDelete = e.translationX < -deleteXThreshold;
+          const shouldDelete = e.translationX < deleteXThreshold;
+          const shouldUpdate = e.translationX > updateXThreshold;
+
+          if (shouldUpdate) {
+            runOnJS(setIsUpdateModalOpen)(true);
+          }
+
           if (shouldDelete) {
             runOnJS(setIsConfirmationModalOpen)(true);
           } else {
             translateX.value = withTiming(0);
           }
         }),
-    [translateX, deleteXThreshold]
+    [translateX, deleteXThreshold, updateXThreshold]
   );
 
   const cardStyle = useAnimatedStyle(() => ({
     transform: [{ translateX: translateX.value }]
   }));
 
-  const iconStyle = useAnimatedStyle(() => ({
-    opacity: withTiming(translateX.value < -deleteXThreshold ? 1 : 0),
+  const editIconStyle = useAnimatedStyle(() => ({
+    opacity: withTiming(translateX.value > updateXThreshold ? 1 : 0),
+    height: 200
+  }));
+
+  const deleteIconStyle = useAnimatedStyle(() => ({
+    opacity: withTiming(translateX.value < deleteXThreshold ? 1 : 0),
     height: 200
   }));
 
@@ -64,14 +80,15 @@ export const Card = ({ goal }: CardProps) => {
   }));
 
   const handleIncrement = () => {
-    updateGoal(id, current < total ? current + 1 : current);
+    updateCurrent(id, current < total ? current + 1 : current);
   };
 
   const handleDecrement = () => {
-    updateGoal(id, current > 0 ? current - 1 : current);
+    updateCurrent(id, current > 0 ? current - 1 : current);
   };
 
   const handleDelete = () => {
+    setIsConfirmationModalOpen(false);
     translateX.value = withTiming(-width);
     itemHeight.value = withTiming(0);
     marginTop.value = withTiming(0);
@@ -82,7 +99,8 @@ export const Card = ({ goal }: CardProps) => {
     });
   };
 
-  const handleCancel = () => {
+  const handleCancelDelete = () => {
+    setIsConfirmationModalOpen(false);
     translateX.value = withTiming(0);
   };
 
@@ -91,9 +109,15 @@ export const Card = ({ goal }: CardProps) => {
       <Animated.View style={containerStyle}>
         <Animated.View
           className={`absolute right-0 top-0 w-24 items-center justify-center`}
-          style={iconStyle}
+          style={deleteIconStyle}
         >
           <Trash size={50} color='#ef4444' />
+        </Animated.View>
+        <Animated.View
+          className={`absolute left-0 top-0 w-24 items-center justify-center`}
+          style={editIconStyle}
+        >
+          <Edit size={50} color='#3B82F6' />
         </Animated.View>
         <GestureDetector gesture={gesture}>
           <Animated.View
@@ -113,9 +137,14 @@ export const Card = ({ goal }: CardProps) => {
       </Animated.View>
       <ConfirmationModal
         open={isConfirmationModalOpen}
-        onClose={() => setIsConfirmationModalOpen(false)}
         onConfirm={handleDelete}
-        onCancel={handleCancel}
+        onClose={handleCancelDelete}
+        onCancel={handleCancelDelete}
+      />
+      <UpdateGoalModal
+        open={isUpdateModalOpen}
+        onClose={() => setIsUpdateModalOpen(false)}
+        goal={goal}
       />
     </>
   );
