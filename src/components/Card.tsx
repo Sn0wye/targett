69631b71bab92cc793ef0f16b9995e986387d9c5
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-01-14 14:29:38 -0300

    feat: implement modal confirmation on deletion

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,6 +1,6 @@
-import { useMemo } from 'react';
-import { View, Text, Dimensions } from 'react-native';
-import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+import { useMemo, useState } from 'react';
+import { Dimensions, Text, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
 import Animated, {
   runOnJS,
   useAnimatedStyle,
@@ -10,6 +10,7 @@ import Animated, {
 
 import { Goal, useGoals } from '../hooks/useGoals';
 import { Actions } from './Actions';
+import { ConfirmationModal } from './ConfirmationModal';
 import { GoalCount } from './GoalCount';
 import { Trash } from './Icons/Trash';
 type CardProps = {
@@ -17,6 +18,7 @@ type CardProps = {
 };
 
 export const Card = ({ goal }: CardProps) => {
+  const [isConfirmationModalOpen, setIsConfirmationModalOpen] = useState(false);
   const { current, total, name, id } = goal;
 
   const { width } = Dimensions.get('window');
@@ -39,28 +41,12 @@ export const Card = ({ goal }: CardProps) => {
         .onEnd(e => {
           const shouldDelete = e.translationX < -deleteXThreshold;
           if (shouldDelete) {
-            translateX.value = withTiming(-width);
-            itemHeight.value = withTiming(0);
-            marginTop.value = withTiming(0);
-            iconOpacity.value = withTiming(0, undefined, isFinished => {
-              if (isFinished) {
-                runOnJS(deleteGoal)(id);
-              }
-            });
+            runOnJS(setIsConfirmationModalOpen)(true);
           } else {
             translateX.value = withTiming(0);
           }
         }),
-    [
-      translateX,
-      deleteXThreshold,
-      width,
-      itemHeight,
-      marginTop,
-      iconOpacity,
-      deleteGoal,
-      id
-    ]
+    [translateX, deleteXThreshold]
   );
 
   const cardStyle = useAnimatedStyle(() => ({
@@ -85,29 +71,52 @@ export const Card = ({ goal }: CardProps) => {
     updateGoal(id, current > 0 ? current - 1 : current);
   };
 
+  const handleDelete = () => {
+    translateX.value = withTiming(-width);
+    itemHeight.value = withTiming(0);
+    marginTop.value = withTiming(0);
+    iconOpacity.value = withTiming(0, undefined, isFinished => {
+      if (isFinished) {
+        runOnJS(deleteGoal)(id);
+      }
+    });
+  };
+
+  const handleCancel = () => {
+    translateX.value = withTiming(0);
+  };
+
   return (
-    <Animated.View style={containerStyle}>
-      <Animated.View
-        className={`absolute right-0 top-0 w-24 items-center justify-center`}
-        style={iconStyle}
-      >
-        <Trash size={50} color='#ef4444' />
-      </Animated.View>
-      <GestureDetector gesture={gesture}>
+    <>
+      <Animated.View style={containerStyle}>
         <Animated.View
-          className='relative rounded-xl bg-gray-800 p-4'
-          style={cardStyle}
+          className={`absolute right-0 top-0 w-24 items-center justify-center`}
+          style={iconStyle}
         >
-          <Text className='text-2xl text-gray-100'>{name}</Text>
-          <View className='mt-4 flex-col items-end justify-between'>
-            <GoalCount current={current} total={total} />
-            <Actions
-              onIncrement={handleIncrement}
-              onDecrement={handleDecrement}
-            />
-          </View>
+          <Trash size={50} color='#ef4444' />
         </Animated.View>
-      </GestureDetector>
-    </Animated.View>
+        <GestureDetector gesture={gesture}>
+          <Animated.View
+            className='relative rounded-xl bg-gray-800 p-4'
+            style={cardStyle}
+          >
+            <Text className='text-2xl text-gray-100'>{name}</Text>
+            <View className='mt-4 flex-col items-end justify-between'>
+              <GoalCount current={current} total={total} />
+              <Actions
+                onIncrement={handleIncrement}
+                onDecrement={handleDecrement}
+              />
+            </View>
+          </Animated.View>
+        </GestureDetector>
+      </Animated.View>
+      <ConfirmationModal
+        open={isConfirmationModalOpen}
+        onClose={() => setIsConfirmationModalOpen(false)}
+        onConfirm={handleDelete}
+        onCancel={handleCancel}
+      />
+    </>
   );
 };
