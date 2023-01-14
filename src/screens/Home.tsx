be32d038522222597429b4a58e68337f044291a5
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-01-14 20:22:10 -0300

    feat: implement new empty state

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -4,6 +4,7 @@ import { Text, TouchableOpacity, View } from 'react-native';
 import { Card } from '../components/Card';
 import { Header } from '../components/Header';
 import { NewGoalModal } from '../components/NewGoalModal';
+import { NoGoals } from '../components/NoGoals';
 import { useGoals } from '../hooks/useGoals';
 
 export const Home = () => {
@@ -19,15 +20,19 @@ export const Home = () => {
     <>
       <View className='h-full w-full'>
         <Header />
-        {goals &&
-          goals.length > 0 &&
-          goals.map(goal => <Card key={goal.id} goal={goal} />)}
-        <TouchableOpacity
-          className='absolute bottom-5 right-2 h-12 w-12 items-center justify-center rounded-lg bg-gray-800'
-          onPress={toggleNewGoalModal}
-        >
-          <Text className='text-4xl text-white'>+</Text>
-        </TouchableOpacity>
+        {goals && goals.length > 0 ? (
+          goals.map(goal => <Card key={goal.id} goal={goal} />)
+        ) : (
+          <NoGoals onCreate={toggleNewGoalModal} />
+        )}
+        {goals && goals.length > 0 && (
+          <TouchableOpacity
+            className='absolute bottom-5 right-2 h-12 w-12 items-center justify-center rounded-lg bg-gray-800'
+            onPress={toggleNewGoalModal}
+          >
+            <Text className='text-4xl text-white'>+</Text>
+          </TouchableOpacity>
+        )}
       </View>
       <NewGoalModal open={isNewGoalModalOpen} onClose={toggleNewGoalModal} />
     </>
