3fb163867cbd99495f1412aaff17997e35e10088
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-05-05 17:36:07 -0300

    feat: add assets and splash

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -1,17 +1,17 @@
 import type { ExpoConfig } from '@expo/config';
 
 const defineConfig = (): ExpoConfig => ({
-  name: 'expo',
-  slug: 'expo',
+  name: 'targett',
+  slug: 'targett',
   scheme: 'expo',
   version: '1.0.0',
   orientation: 'portrait',
   icon: './assets/icon.png',
-  userInterfaceStyle: 'light',
+  userInterfaceStyle: 'dark',
   splash: {
-    image: './assets/icon.png',
+    image: './assets/splash.png',
     resizeMode: 'contain',
-    backgroundColor: '#1F104A'
+    backgroundColor: '#121214'
   },
   updates: {
     fallbackToCacheTimeout: 0
@@ -19,17 +19,21 @@ const defineConfig = (): ExpoConfig => ({
   assetBundlePatterns: ['**/*'],
   ios: {
     supportsTablet: true,
-    bundleIdentifier: 'your.bundle.identifier'
+    bundleIdentifier: 'com.sn0wye.targett'
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: './assets/icon.png',
-      backgroundColor: '#1F104A'
-    }
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#FFFFFF'
+    },
+    package: 'com.sn0wye.targett'
+  },
+  web: {
+    favicon: './assets/favicon.png'
   },
   extra: {
     eas: {
-      // projectId: "your-project-id",
+      projectId: '22751e82-985c-495e-aea7-1a53241f86e8'
     },
     publishableKey:
       'pk_test_c21hc2hpbmctdmVydmV0LTkyLmNsZXJrLmFjY291bnRzLmRldiQ'
