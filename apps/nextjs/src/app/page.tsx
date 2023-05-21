ec77d9546cb278ad925078ef6f1ebb08d6d56ad7
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-05-21 15:42:29 -0300

    feat: temporary working web query

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -1,11 +1,25 @@
+import { currentUser } from '@clerk/nextjs/app-beta';
+
+import { GoalService } from '@targett/api/src/router/services/GoalService';
+import { db } from '@targett/db';
+import { type ParsedGoal } from '@targett/db/schemas';
+
 import { GoalCards } from '~/components/goal-cards';
 import { HomeActions } from '~/components/home-actions';
 
-const DashboardPage = () => {
+const goalService = new GoalService(db);
+const DashboardPage = async () => {
+  const user = await currentUser();
+
+  let goals: ParsedGoal[] = [];
+  if (user) {
+    goals = await goalService.all(user.id);
+  }
+
   return (
-    <div className='flex flex-col gap-6 pb-12 pt-6'>
+    <div className='mb-12 mt-6 flex flex-col gap-6 px-6'>
       <HomeActions />
-      <GoalCards />
+      <GoalCards goals={goals} />
     </div>
   );
 };
--- apps/nextjs/src/components/goal-cards.tsx
+++ apps/nextjs/src/components/goal-cards.tsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import { CalendarDays } from 'lucide-react';
 
-export const GoalCards = () => {
+import { type ParsedGoal } from '@targett/db/schemas';
+
+type GoalCardsProps = {
+  goals: ParsedGoal[];
+};
+
+export const GoalCards = ({ goals }: GoalCardsProps) => {
   return (
     <div className='grid cursor-pointer grid-cols-3 gap-6'>
-      {Array.from({ length: 5 }).map((_, i) => (
+      {goals.map(goal => (
         <div
-          key={i}
-          className='rounded-md border border-zinc-800 bg-zinc-950 p-4 transition-[border-color_150ms_ease] hover:border-zinc-300'
+          key={goal.id}
+          className='rounded-md border border-zinc-800 bg-zinc-950 p-6 transition-[border-color_150ms_ease] hover:border-zinc-300'
         >
-          Goal {i + 1}
+          <header className='flex justify-between'>
+            <div className='flex flex-col'>
+              <h3 className='font-medium text-zinc-100'>{goal.name}</h3>
+              <p className='text-zinc-400'>
+                {goal.current} / {goal.total}
+              </p>
+            </div>
+            <CalendarDays className='h-6 w-6' />
+          </header>
         </div>
       ))}
     </div>
