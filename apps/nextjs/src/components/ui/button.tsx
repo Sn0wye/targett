0454ec170059b9aa76942b525babfaf2edcca496
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-07-08 11:13:26 -0300

    feat: add ui button

--- apps/nextjs/src/components/ui/button.tsx
+++ apps/nextjs/src/components/ui/button.tsx
@@ -3,11 +3,12 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '~/lib/utils';
+import { LoadingDots } from './loading-dots';
 
 //TODO: add secondary and destructive variants
 
 const buttonVariants = cva(
-  'flex items-center rounded-md text-sm border font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-100 focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background',
+  'flex items-center rounded-md text-sm border font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-zinc-100 focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background group',
   {
     variants: {
       variant: {
@@ -39,17 +40,32 @@ export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
+  isLoading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  (
+    {
+      children,
+      className,
+      variant,
+      size,
+      asChild = false,
+      isLoading = false,
+      ...props
+    },
+    ref
+  ) => {
     const Comp = asChild ? Slot : 'button';
+    const content = isLoading ? <LoadingDots variant='light' /> : children;
     return (
       <Comp
         className={cn(buttonVariants({ variant, size, className }))}
         ref={ref}
         {...props}
-      />
+      >
+        {content}
+      </Comp>
     );
   }
 );
