8fee660a2952c4b11698d84e76d09c7ff6661591
Gabriel Trzimajewski <gabrieltrz2005@gmail.com>
2023-07-08 11:14:40 -0300

    feat: add home actions

--- apps/nextjs/src/components/home-actions.tsx
+++ apps/nextjs/src/components/home-actions.tsx
@@ -1,18 +1,46 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
+import { useUser } from '@clerk/nextjs';
 import { Plus, Search } from 'lucide-react';
 
+import { Button } from '~/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger
+} from '~/components/ui/dropdown-menu';
 import { Input } from '~/components/ui/input';
-import { Button } from './ui/button';
+import { CreateGoalDialog } from './create-goal-dialog';
 
 export const HomeActions = () => {
+  const [isCreateGoalDialogOpen, setIsCreateGoalDialogOpen] = useState(false);
+  const { isSignedIn } = useUser();
+
   return (
     <div className='flex gap-2'>
       <Input leftIcon={<Search />} placeholder='Pesquise...' />
-      <Button>
-        Novo... <Plus className='ml-2 h-5 w-5' />
-      </Button>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button>
+            New... <Plus className='ml-2 h-5 w-5' />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent className='w-32' align='end'>
+          <DropdownMenuItem
+            onClick={() => setIsCreateGoalDialogOpen(true)}
+            disabled={!isSignedIn}
+          >
+            Goal
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <CreateGoalDialog
+        open={isCreateGoalDialogOpen}
+        onOpenChange={setIsCreateGoalDialogOpen}
+      />
     </div>
   );
 };
